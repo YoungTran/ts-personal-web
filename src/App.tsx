94f06714f2e7b7ae41e1a8ba565a0dcc8fe4ec1b
YoungTran <>
2021-07-21 21:17:55 -0700

    impl history and change containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import { motion } from 'framer-motion';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Contact from './components/Contact';
 import Experience from './components/Experience/Experience';
 import Footer from './components/Footer';
@@ -11,8 +12,12 @@ import ShowcaseList from './components/Showcase';
 import useApp from './hooks/useApp';
 
 const StyledApp = styled.div`
+  padding: 1rem clamp(2em, 14%, 13.5em);
   position: relative;
   height: 100%;
+  @media (max-width: 768px) {
+    padding: 1rem 3rem;
+  }
 `;
 
 const Logo42 = styled(motion.img)<{ animClass: boolean }>`
@@ -30,43 +35,42 @@ const Logo42 = styled(motion.img)<{ animClass: boolean }>`
   }
 `;
 
-const PageWrapper = styled.div`
+const AppContainer = styled.div`
   background-color: var(--bg);
-  padding: 1rem clamp(2em, 15%, 13.5em);
-  @media (max-width: 768px) {
-    padding: 1rem 3rem;
-  }
 `;
 
 const App = (): JSX.Element => {
   const { loading, animClass, repos, error } = useApp();
+  const history = useHistory();
+
   return (
-    <>
+    <AppContainer className="bg-gradient-to-tr from-gray-900">
       <motion.div animate={{ opacity: loading ? 1 : 0 }}>
         <FullScreenLoader />
       </motion.div>
-
-      <PageWrapper className="bg-gradient-to-tr from-gray-900">
-        {!loading && !error && (
-          <StyledApp>
-            <Header />
-            <Hero />
-            <Logo42
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 1.6 }}
-              src="/42.png"
-              animClass={animClass}
-              className="sm:mr-60 mr-14"
-            />
-            <Experience />
-            <ShowcaseList repos={repos} />
-            <Contact />
-            <Footer />
-          </StyledApp>
-        )}
-      </PageWrapper>
-    </>
+      <Header />
+      {!loading && !error && (
+        <StyledApp>
+          <Hero />
+          <Logo42
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.6 }}
+            src="/42.png"
+            animClass={animClass}
+            className="sm:mr-48 mr-14 cursor-pointer"
+            onClick={() => {
+              window.scrollTo(0, 0);
+              history.replace('/');
+            }}
+          />
+          <Experience />
+          <ShowcaseList repos={repos} />
+          <Contact />
+          <Footer />
+        </StyledApp>
+      )}
+    </AppContainer>
   );
 };
 
